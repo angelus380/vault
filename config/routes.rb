25d2e199a13d5fc8eb7eac1da578e7194e7a4360
angelus380 <angelusdelmar@gmail.com>
2016-05-05 11:34:16 +0100

    create routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get '/index' => 'welcome#index'
   get '/contact' => 'welcome#contact'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
