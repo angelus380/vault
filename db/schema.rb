a5ed4c55ee7469b931b89ace082302d9bd4ce097
angelus380 <angelusdelmar@gmail.com>
2016-05-06 12:36:38 +0100

    changed and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@
 ActiveRecord::Schema.define(version: 20160505100029) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.string   "name"
-    t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
