5a404be46279857bad68b265056539654724e7d5
angelus380 <angelusdelmar@gmail.com>
2016-05-07 22:58:57 +0100

    validates username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :username, :name, presence: true
+  validates :username, uniqueness: true, presence: true
+  validates :name, presence: true
 end
